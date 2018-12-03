9d29fa6aef60ef5f99e8871dc81a2c5624d92676
Lucas Costa <lc.benjamin@gmail.com>
2018-12-03 01:11:03 -0300

    delete de imagens

--- view/front/pacotes.php
+++ view/front/pacotes.php
@@ -1,5 +1,18 @@
+<?php
+    /** Includes e conexões com o banco */    
+    require_once '../../config.php';
+    require_once DBAPI;
 
-<!-- Pacotes nacionais-->
+    require_once('../../controller/beans/pacotesBean.php');
+    require_once('../../controller/beans/estadoBean.php');
+    require_once('../../controller/beans/cidadeBean.php');
+    require_once('../../controller/beans/telaPrincipalBean.php');
+    carrega_pacotes();
+
+?>
+
+
+<!-- Pacotes de viagem-->
 <div class="row" id="tituloPacotes">
     <div class="col-12 text-left my-3">
         <h2 class="display-6 text-dark">
@@ -8,24 +21,36 @@
         </h2>
     </div>
 </div>
-    <div class="row mb-5">
-        <?php
-        for($i = 0 ; $i < 9 ; $i++){
-            echo '
-            <div class="col-sm-3">
-                <div class="card my-3">
-                    <img src="../imagens/diversas/paris.jpg" class="card-img-top">
-                    <div class="card-body">
-                        <h4 class="card-title">Título 1</h4>
-                        <h6 class="card-title mb-2 text-muted">SubTítulo 1</h6>
-                        <p class="card-text">Lorem Ipsum é simplesmente uma simulação de texto da indústria tipográfica</p>
-                        <div class="card-body text-right">
-                            <button type="button" class="btn btn-secondary">Comprar</button>
-                        </div>
+  
+
+<div class="row mb-5">
+    <?php foreach ($pacotes as $pacote) : ?>
+        <!-- Consulta as imagens associadas ao pacote -->
+        <?php $imagensPacote = consultaIMGporIdPCT($pacote['codPacote']); ?>
+    
+        <div class="col-sm-3">
+            <div class="card my-3">
+                <img src="../imagens/img_pacotes/<?php echo $imagensPacote[1]['nome']; ?>" class="card-img-top">
+                <div class="card-body">
+                    <h4 class="card-title"><?php echo $pacote['titulo']; ?></h4>
+                    <h6 class="card-title mb-2 text-muted"><?php echo caculaDiarias($pacote['dataInicio'],$pacote['dataFim']) . " Dias" ; ?></h6>
+                    <p class="card-text">
+                        <?php
+                            echo "Por apenas R$ " . 
+                            number_format(calculaValorTotalPacote($pacote), 2, ',', '.') . "<br>" ;
+                            if($pacote['parcela']){
+                                echo " em " . $pacote['quantidadeParcelas'] . " parcelas";
+                            } else {
+                                echo "A vista";
+                            }
+                        ?>
+                    </p>                    
+                    <div class="card-body text-right">
+                        <a href="./index.php?p=detalhePacote&id=<?php echo $pacote['codPacote'];?>" class="btn btn-secondary">Eu quero!</a>
                     </div>
                 </div>
             </div>
-            ';
-        }
-        ?>
-    </div>
+        </div>
+        
+    <?php endforeach ; ?>
+</div>
