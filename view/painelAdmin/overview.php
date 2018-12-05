0c106e569a151f9983e039d255fc0a0950f23d9c
lcbenjamin <lc.benjamin@gmail.com>
2018-12-05 16:58:29 -0300

    Overview

--- view/painelAdmin/overview.php
+++ view/painelAdmin/overview.php
@@ -17,13 +17,45 @@
 	</div>
 </header>
 
+
+          <div class="row tile_count">
+            <div class="col-md-2 col-sm-4 col-xs-6 tile_stats_count">
+              <span class="count_top"><i class="fa fa-user"></i> Total Users</span>
+              <div class="count">2500</div>
+              <span class="count_bottom"><i class="green">4% </i> From last Week</span>
+            </div>
+            <div class="col-md-2 col-sm-4 col-xs-6 tile_stats_count">
+              <span class="count_top"><i class="fa fa-clock-o"></i> Average Time</span>
+              <div class="count">123.50</div>
+              <span class="count_bottom"><i class="green"><i class="fa fa-sort-asc"></i>3% </i> From last Week</span>
+            </div>
+            <div class="col-md-2 col-sm-4 col-xs-6 tile_stats_count">
+              <span class="count_top"><i class="fa fa-user"></i> Total Males</span>
+              <div class="count green">2,500</div>
+              <span class="count_bottom"><i class="green"><i class="fa fa-sort-asc"></i>34% </i> From last Week</span>
+            </div>
+            <div class="col-md-2 col-sm-4 col-xs-6 tile_stats_count">
+              <span class="count_top"><i class="fa fa-user"></i> Total Females</span>
+              <div class="count">4,567</div>
+              <span class="count_bottom"><i class="red"><i class="fa fa-sort-desc"></i>12% </i> From last Week</span>
+            </div>
+            <div class="col-md-2 col-sm-4 col-xs-6 tile_stats_count">
+              <span class="count_top"><i class="fa fa-user"></i> Total Collections</span>
+              <div class="count">2,315</div>
+              <span class="count_bottom"><i class="green"><i class="fa fa-sort-asc"></i>34% </i> From last Week</span>
+            </div>
+            <div class="col-md-2 col-sm-4 col-xs-6 tile_stats_count">
+              <span class="count_top"><i class="fa fa-user"></i> Total Connections</span>
+              <div class="count">7,325</div>
+              <span class="count_bottom"><i class="green"><i class="fa fa-sort-asc"></i>34% </i> From last Week</span>
+            </div>
+          </div>
+
+
 <div class="row bg-light">
     <div id="piechart" style="width: 450px; height: 250px;"></div>
+    <div id="chart_div" style="width: 450px; height: 250px;"></div>
 </div>
-
-<div id="chart_div" style="width: 900px; height: 500px;"></div>
-
-
 
 
 <script type="text/javascript">
@@ -68,6 +100,7 @@
 
     var options = {
         region: 'BR',
+        title: 'Situação Pacotes',
         resolution: 'provinces',
         displayMode: 'regions',
         colorAxis: {colors: ['yellow', 'red']}
